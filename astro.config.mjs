d188554dfc77e2c79e2225de9ea276088188df36
Josh Muszka <josh@jmuszka.com>
2025-01-01 17:56:10 -0500

    Fixed image-rendering bug on production configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import node from '@astrojs/node';
 
-
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://www.jmuszka.com',
+  output: 'static',
   integrations: [react(), tailwind()],
-  i18n: {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-    routing: {
-      prefixDefaultLocale: false
-    }
-  },
-  output: 'server',
-  // adapter: vercel({
-  //   imageService: true,
-  // }),
-  adapter: node({
-    mode: 'production',
-  }),
-});
+})
