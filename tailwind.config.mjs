bdd4a889c635f680e421f297e88bc27a6ed445fb
jmuszka <josh@jmuszka.com>
2025-01-13 15:30:07 -0500

    Hand emoji waving animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'0%': {transform: 'rotate(0.0deg)'},
+				'15%': {transform: 'rotate(14.0deg)'},
+				'30%': {transform: 'rotate(-8.0deg)'},
+				'40%': {transform: 'rotate(14.0deg)'},
+				'50%': {transform: 'rotate(-4.0deg)'},
+				'60%': {transform: 'rotate(10.0deg)'},
+				'70%': {transform: 'rotate(0.0deg)'},
+				'100%': {transform: 'rotate(0.0deg)'},
+			},
+			animation: {
+				wave: 'wave 1.5s infinite'
+			}
+		},
 	},
 	plugins: [],
 }
